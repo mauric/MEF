f5892d273681f30490ba0476062c7b905511490e
mauric <mauric.caceres@gmail.com>
2016-05-21 15:08:14 +0200

    Première commit, initialisation du projet

---
+++ point.h
@@ -0,0 +1,34 @@
+#ifndef POINTS_H
+#define POINTS_H
+
+#include <iostream>
+#include <cstdlib>
+#include <math.h>
+#include <fstream>
+
+
+using namespace std;
+
+typedef struct
+{	
+	double num;
+	double x;
+	double y;
+} P;
+
+typedef struct
+{
+	double num;
+	string n;
+	double pt1;
+	double pt2;
+	double Xc;
+	double Yc;
+} S;
+
+
+struct L
+{	string lg;
+};
+
+#endif
